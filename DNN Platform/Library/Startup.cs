4096f3326e152f2b6c91abaa8cf31b9458f2b0be
Peter Donker <peter@bring2mind.net>
2026-02-28 22:17:41 +0100

    Correct path for config file and add to DNN startup

--- DNN Platform/Library/Startup.cs
+++ DNN Platform/Library/Startup.cs
@@ -75,6 +75,9 @@ namespace DotNetNuke
         {
             services.AddTransient(typeof(Lazy<>), typeof(LazyWrapper<>));
 
+            var hostMapPath = System.Web.Hosting.HostingEnvironment.MapPath("~");
+            services.AddSerilog(hostMapPath);
+
             services.TryAddEnumerable(ServiceDescriptor.Singleton<IModuleControlFactory, WebFormsModuleControlFactory>());
             services.TryAddEnumerable(ServiceDescriptor.Singleton<IModuleControlFactory, Html5ModuleControlFactory>());
             services.TryAddEnumerable(ServiceDescriptor.Singleton<IModuleControlFactory, ReflectedModuleControlFactory>());
