51a0998eb4c2be9387cacb0dc8fd5e3524621922
Peter Donker <peter@bring2mind.net>
2026-03-01 22:35:52 +0100

    Fix position of adding logger in jwt controller

--- DNN Platform/Dnn.AuthServices.Jwt/Components/Common/Controllers/JwtController.cs
+++ DNN Platform/Dnn.AuthServices.Jwt/Components/Common/Controllers/JwtController.cs
@@ -47,8 +47,8 @@ namespace Dnn.AuthServices.Jwt.Components.Common.Controllers
         /// <summary>Initializes static members of the <see cref="JwtController"/> class.</summary>
         static JwtController()
         {
-            ValidateConfiguration();
             Logger = DnnLoggingController.GetLogger<JwtController>();
+            ValidateConfiguration();
         }
 
         /// <inheritdoc />
