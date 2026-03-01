678e228a4d1a9ee50f02afc000b186aa78f629b2
Peter Donker <peter@bring2mind.net>
2026-03-01 12:09:22 +0100

    Update DNN Platform/Dnn.AuthServices.Jwt/Components/Common/Controllers/JwtController.cs
    Co-authored-by: Mitchel Sellers <msellers@Iowacomputergurus.com>

--- DNN Platform/Dnn.AuthServices.Jwt/Components/Common/Controllers/JwtController.cs
+++ DNN Platform/Dnn.AuthServices.Jwt/Components/Common/Controllers/JwtController.cs
@@ -96,7 +96,7 @@ namespace Dnn.AuthServices.Jwt.Components.Common.Controllers
         {
             if (!JwtAuthMessageHandler.IsEnabled)
             {
-                Logger.LogTrace(this.SchemeType + " is not registered/enabled in web.config file");
+                Logger.LogTrace("{SchemeType} is not registered/enabled in web.config file", this.SchemeType);
                 return null;
             }
 
